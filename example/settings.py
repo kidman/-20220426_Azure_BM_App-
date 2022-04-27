e088d97e6194ff0912189b82de535d1540a05fb8
vijaykumar3060 <Vijaykumar3060@hotmail.com>
2022-04-27 16:10:16 -0400

    Upadted for WhiteNoise module

--- example/settings.py
+++ example/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "%lam$s$t8t%8#@uk+fx=xm0x*@ccj5d0&z^8#ku4gb$lk@pfxt"
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = 'static'
+
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'whotenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 for settings_file in ["local_settings"]:
